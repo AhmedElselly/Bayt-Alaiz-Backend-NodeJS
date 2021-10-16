7ba77b86655f769e8ef7c375c6fdc401e9c12ece
ahmedelselly <ahmedelselly87@gmail.com>
2021-10-16 08:47:37 +0200

    updated app.js and added start: node app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const LocalStrategy = require('passport-local');
 
 const User = require('./models/user');
 
-mongoose.connect('mongodb+srv://elselly:1234Selly301060@cluster0.6ybi9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
