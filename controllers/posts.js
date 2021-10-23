7b03505356422cdaa1d269a1e1ec9baa362fd8e2
ahmedelselly <ahmedelselly87@gmail.com>
2021-10-23 14:08:13 +0200

    console.log(search)

--- controllers/posts.js
+++ controllers/posts.js
@@ -72,7 +72,7 @@ module.exports = {
 
 	async findPost(req, res){
 		const posts = await Post.find({$or: [{name: req.body.search}, {description: req.body.search}]}).select('-image');
-
+		console.log(req.body.search)
 		console.log(posts)
 		return res.json(posts);
 	},
